372960ffa2720b73b2b030200c392fd6e2881b99
AbderrafiiGoutaib <GOUTAIB.ABDERRAFII@emsi-edu.ma>
2020-09-15 21:25:10 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from './Components/Header/Header.jsx'
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage.component.jsx";
 import ShopPage from "./Pages/Shop/ShopPage.jsx";
@@ -7,6 +8,7 @@ import ShopPage from "./Pages/Shop/ShopPage.jsx";
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
