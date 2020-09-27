838d16de300d82a8bb221bd794778921e00bb8cc
AbderrafiiGoutaib <63517761+AbderrafiiGoutaib@users.noreply.github.com>
2020-09-27 01:40:38 +0100

    added selectors with reselect library

--- src/Components/Cart-dropdown/Cart-dropdown.jsx
+++ src/Components/Cart-dropdown/Cart-dropdown.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Cart-dropdown.scss";
 import CustomButton from "../Custom-button/Custom-button";
 import CartItem from "../CartItem/CartItem";
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 import { connect } from "react-redux";
 
 const CartDropdown = ({ cartItems }) => {
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => {
     </div>
   );
 };
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems:selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
